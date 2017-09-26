5655dbceb7e952bff2fa687e6f508caf8550bfea
Ditte <Ditte@DESKTOP-QORFADN>
2017-09-26 11:53:04 +0200

    TJEK

--- RepoTjek/Service1.svc.cs
+++ RepoTjek/Service1.svc.cs
@@ -12,6 +12,10 @@ namespace RepoTjek
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
+
+
+        //TJEK TIL GITHUB HÅNDTERING
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
